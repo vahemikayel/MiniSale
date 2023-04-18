92a1c9290e073b34dfdcb95914d6c07fa5c54a9f
Vahe Mikayelyan <vahemikayel@gmail.com>
2023-04-18 20:08:51 +0400

    Add project files.

---
+++ MiniSale.Interfaces/IProduct.cs
@@ -0,0 +1,24 @@
+﻿namespace MiniSale.Interfaces
+{
+    public interface IProduct
+    {
+        Guid Id { get; set; }
+
+        /// <summary>
+        /// 5-15 characters 
+        /// </summary>
+        string Name { get; set; }
+
+        decimal Price { get; set; }
+
+        /// <summary>
+        /// Can be null or exactly contain 13 characters and be unique
+        /// </summary>
+        string BarCode { get; set; }
+
+        /// <summary>
+        /// Must be in range 1-99999 and unique
+        /// </summary>
+        int PLU { get; set; }
+    }
+}
